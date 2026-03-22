33b6bd604ae4cfad3dab5501fac498cdac8a0913
Михаил Казаков <mishakazakov82@gmail.com>
2026-03-22 15:19:04 +0500

    Добавьте файлы проекта.

---
+++ Models/Messages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents_Kazakov.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public int UserFrom { get; set; }
+        public int UserTo { get; set; }
+        public string Message { get; set; }
+        public Messages(int userFrom, int userTo, string Message)
+        {
+            this.UserFrom = userFrom;
+            this.UserTo = userTo;
+            this.Message = Message;
+        }
+    }
+}
